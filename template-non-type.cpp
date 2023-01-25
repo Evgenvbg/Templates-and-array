d4e4189048e9db6a8dceaa032d021cc6df0a5c23
Evgeniy Rudenya <Evgru1@yandex.ru>
2023-01-25 20:18:59 +0300

    Add files via upload

---
+++ template-non-type.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+template <typename T, int size>
+class Array {
+private:
+
+    T array[size];
+
+public:
+    T* getLenght();
+    T& operator[](T size)
+    {
+        return array[size];
+    }
+
+    
+
+};
+    template <typename T, int size>
+    T* Array <T, size> :: getLenght()
+    {
+    return array;
+    }
+
+    int main()
+    {
+        Array<int, 10> arr;
+        for (int i = 0; 10 > i; i++)
+        {
+            arr[i] = i;
+        }
+
+        for (int i = 0; 10 > i; i++)
+        {
+            std::cout << arr[i];
+        }
+
+        return 0;
+    }
+
---
+++ template_array.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+template <typename T>
+class Array {
+private:
+
+    T* array;
+    int length;
+
+public:
+    Array()
+    {
+        array = nullptr;
+        length = 0;
+    }
+
+    Array(int length)
+    {
+        T* array = new T(length);
+        this->length = length;
+    }
+
+    
+
+    T& operator[](int size)
+    {
+        if (size > 0 && size < length)
+        return array[size];
+    }
+
+    void erase()
+    {
+        delete[] array;
+        array = nullptr;
+        length = 0;
+    }
+    
+
+    ~Array() { delete[] array; }
+    int getLength();
+
+};
+    template <typename T>
+    int Array <T> :: getLength()
+    {
+    return length;
+    }
+
+    int main()
+    {
+        Array<int> arr(10);
+        for (int i = 0; arr.getLength() > i; i++)
+        {
+            arr[i] = i;
+        }
+
+        for (int i = 0; 10 > i; i++)
+        {
+            std::cout << arr[i];
+        }
+
+        return 0;
+    }
